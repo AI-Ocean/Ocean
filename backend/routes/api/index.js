89c455fcc8866ec2b6f874d85839ed3648acac5b
kairos03 <kairos9603@gmail.com>
2020-06-01 08:26:14 +0000

    Revert "Merge branch 'feature/2003-improve-dashboard' into 'master'"
    This reverts merge request !18

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,7 @@
 var router = require('express').Router()
 
 router.use('/notice', require('./notice'))
+router.use('/schedules', require('./schedules'))
 router.use('/resources', require('./resources'))
 router.use('/instances', require('./instances'))
 router.use('/volumes', require('./volumes'))
