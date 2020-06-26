0c3735293047328a5bc63fa4b0d80f4315bdad14
kairos03 <kairos9603@gmail.com>
2020-06-26 13:09:27 +0900

    express static files relative import

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join('public')))
 
 app.use(require('./middlewares/verifyToken'))
 
@@ -47,7 +47,6 @@ app.use(function(err, req, res, next) {
 
   // send the error
   res.status(err.status || 500).send(err.message || 'ERROR')
-  // res.render('error')
 });
 
 module.exports = app
