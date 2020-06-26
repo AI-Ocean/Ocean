0932f4ba2dabac4f6756b5cd97004124ccd0419a
kairos03 <kairos9603@gmail.com>
2020-06-26 13:28:47 +0900

    vue relative path for static files

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join('public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(require('./middlewares/verifyToken'))
 
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -22,5 +22,6 @@ module.exports = {
   'transpileDependencies': [
     'vuetify'
   ],
+  publicPath: '',
   outputDir: './dist'
 }
