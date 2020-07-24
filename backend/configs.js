8551ff4561f1337c65b8d8c48eb8692f3cd48661
kairos03 <kairos9603@gmail.com>
2020-07-24 17:57:08 +0900

    decouple configs

---
+++ backend/configs.js
@@ -0,0 +1,10 @@
+var path = require('path')
+var dotenv = require('dotenv')
+dotenv.config({ path: path.join(__dirname, '.env') })
+
+module.exports = {
+  KUBE_HOST: process.env.KUBE_HOST || "localhost",
+  KUBE_PORT: process.env.KUBE_PORT || 6443,
+  KUBE_TOKEN: process.env.KUBE_TOKEN,
+  FIREBASE_CERT: JSON.parse(process.env.FIREBASE_CERT)
+}
