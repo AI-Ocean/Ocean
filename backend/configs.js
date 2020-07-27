b4a0ac5c28142590f0090478de0bca24bcd272de
kairos03 <kairos9603@gmail.com>
2020-07-27 15:48:06 +0900

    add conditional env load

--- backend/configs.js
+++ backend/configs.js
@@ -1,10 +1,13 @@
 var path = require('path')
 var dotenv = require('dotenv')
-dotenv.config({ path: path.join(__dirname, '.env') })
+
+if (process.env.NODE_ENV='develop') {
+  dotenv.config({ path: path.join(__dirname, '.env') })
+}
 
 module.exports = {
   KUBE_HOST: process.env.KUBE_HOST || "localhost",
   KUBE_PORT: process.env.KUBE_PORT || "6443",
-  KUBE_TOKEN: String(process.env.KUBE_TOKEN),
+  KUBE_TOKEN: process.env.KUBE_TOKEN,
   FIREBASE_CERT: JSON.parse(process.env.FIREBASE_CERT)
 }
