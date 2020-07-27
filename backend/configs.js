0d0574adffccc9faf5725afaceb1d4c1915dbce6
kairos03 <kairos9603@gmail.com>
2020-07-27 15:04:19 +0900

    casting KUBE_TOKEN to String

--- backend/configs.js
+++ backend/configs.js
@@ -4,7 +4,7 @@ dotenv.config({ path: path.join(__dirname, '.env') })
 
 module.exports = {
   KUBE_HOST: process.env.KUBE_HOST || "localhost",
-  KUBE_PORT: process.env.KUBE_PORT || 6443,
-  KUBE_TOKEN: process.env.KUBE_TOKEN,
+  KUBE_PORT: process.env.KUBE_PORT || "6443",
+  KUBE_TOKEN: String(process.env.KUBE_TOKEN),
   FIREBASE_CERT: JSON.parse(process.env.FIREBASE_CERT)
 }
