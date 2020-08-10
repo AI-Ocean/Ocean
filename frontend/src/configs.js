31db71923b24e7b7a55a668d558ff0cb81673e51
kairos03 <kairos9603@gmail.com>
2020-08-10 17:36:01 +0900

    check frontend configs

--- frontend/src/configs.js
+++ frontend/src/configs.js
@@ -1,7 +1,5 @@
 const firebaseConfig = eval('(' + process.env.VUE_APP_FIREBASE_CONFIG + ')')
 
-console.log(process.env.VUE_APP_FIREBASE_CONFIG)
-
 module.exports = {
   FIREBASE_CONFIG: firebaseConfig
 }
