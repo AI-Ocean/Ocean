77ffe8d58376829143a84dbd2016511e36604493
kairos03 <kairos9603@gmail.com>
2020-08-10 19:26:10 +0900

    fix frontend configs loading error

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const Sentry = require('@sentry/node')
 Sentry.init({ dsn: 'https://a05fb6559e15481e88528d4f32642757@sentry.io/5174189' })
 
+var configs = require('./configs')
+
 var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
@@ -9,9 +11,12 @@ var logger = require('morgan')
 var admin = require('firebase-admin')
 var history = require('connect-history-api-fallback');
 
-var configs = require('./configs')
-
 var app = express();
+// config files
+app.use('/config.js', (req, res) => {
+  res.set('Content-type', 'text/javascript')
+  res.send(`window.FIREBASE_CONFIG = ` + JSON.stringify(process.env.FIREBASE_CONFIG))
+})
 
 // firebase
 admin.initializeApp({
@@ -20,13 +25,11 @@ admin.initializeApp({
 
 // middleware
 app.use(history())
-
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-
 app.use(require('./middlewares/verifyToken'))
 
 // router
