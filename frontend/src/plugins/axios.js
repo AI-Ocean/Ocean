f3c811ca361bf91a9f26bc35b4cda5432e58c6aa
kairos03 <kairos9603@gmail.com>
2020-09-14 11:38:32 +0900

    add token refresh logic

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -35,4 +35,15 @@ localAPI.interceptors.request.use((req) => {
   return Promise.reject(error)
 })
 
+localAPI.interceptors.response.use((res) => {
+  return res
+}, async (err) => {
+  if (err.response.status === 403 && err.config.resend === undefined) {
+    err.config.resend = true
+    await store.dispatch('getUser', store.state.user)
+    return localAPI(err.config)
+  }
+  return Promise.reject(err)
+})
+
 Vue.prototype.$axios = localAPI
